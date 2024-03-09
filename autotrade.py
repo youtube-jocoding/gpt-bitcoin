235f85155f916a6621bb36766312fe9f89bf320c
youtube-jocoding <adjocoding@gmail.com>
2024-03-09 15:55:37 +0900

    first commit

---
+++ autotrade.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from openai import OpenAI
+import pyupbit
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+upbit = pyupbit.Upbit(os.getenv("UPBIT_ACCESS_KEY"), os.getenv("UPBIT_SECRET_KEY"))
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
+krw = upbit.get_balance("KRW")
+print(krw)
