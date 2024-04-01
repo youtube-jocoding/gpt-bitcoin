7fb0a2fc6368c48b0265b79a3e30a142e6443b9a
youtube-jocoding <adjocoding@gmail.com>
2024-04-01 18:01:24 +0900

    SerpApi Key Error fix

--- autotrade_v2.py
+++ autotrade_v2.py
@@ -156,26 +156,33 @@ def fetch_and_prepare_data():
 def get_news_data():
     ### Get news data from SERPAPI
     url = "https://serpapi.com/search.json?engine=google_news&q=btc&api_key=" + os.getenv("SERPAPI_API_KEY")
-    response = requests.get(url)
-    news_results = response.json()['news_results']
 
-    simplified_news = []
-    
-    for news_item in news_results:
-        # Check if this news item contains 'stories'
-        if 'stories' in news_item:
-            for story in news_item['stories']:
-                timestamp = int(datetime.strptime(story['date'], '%m/%d/%Y, %H:%M %p, %z %Z').timestamp() * 1000)
-                simplified_news.append((story['title'], story.get('source', {}).get('name', 'Unknown source'), timestamp))
-        else:
-            # Process news items that are not categorized under stories but check date first
-            if news_item.get('date'):
-                timestamp = int(datetime.strptime(news_item['date'], '%m/%d/%Y, %H:%M %p, %z %Z').timestamp() * 1000)
-                simplified_news.append((news_item['title'], news_item.get('source', {}).get('name', 'Unknown source'), timestamp))
+    result = "No news data available."
+
+    try:
+        response = requests.get(url)
+        news_results = response.json()['news_results']
+
+        simplified_news = []
+        
+        for news_item in news_results:
+            # Check if this news item contains 'stories'
+            if 'stories' in news_item:
+                for story in news_item['stories']:
+                    timestamp = int(datetime.strptime(story['date'], '%m/%d/%Y, %H:%M %p, %z %Z').timestamp() * 1000)
+                    simplified_news.append((story['title'], story.get('source', {}).get('name', 'Unknown source'), timestamp))
             else:
-                simplified_news.append((news_item['title'], news_item.get('source', {}).get('name', 'Unknown source'), 'No timestamp provided'))
-    
-    return str(simplified_news)
+                # Process news items that are not categorized under stories but check date first
+                if news_item.get('date'):
+                    timestamp = int(datetime.strptime(news_item['date'], '%m/%d/%Y, %H:%M %p, %z %Z').timestamp() * 1000)
+                    simplified_news.append((news_item['title'], news_item.get('source', {}).get('name', 'Unknown source'), timestamp))
+                else:
+                    simplified_news.append((news_item['title'], news_item.get('source', {}).get('name', 'Unknown source'), 'No timestamp provided'))
+        result = str(simplified_news)
+    except Exception as e:
+        print(f"Error fetching news data: {e}")
+
+    return result
 
 def fetch_fear_and_greed_index(limit=1, date_format=''):
     """
